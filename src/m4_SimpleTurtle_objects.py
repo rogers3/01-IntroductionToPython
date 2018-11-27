8e4892e34c6573f8795ba270b252273c90eb3b1f
rogerscj <rogerscj@rose-hulman.edu>
2018-11-26 19:59:55 -0500

    done

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving,
 #      ** click anywhere in the window to close the window **.
@@ -116,7 +116,7 @@ natasha.go_to(rg.Point(-100, 200))
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more lines of your own code to make one of the existing
 #   SimpleTurtles move some more and/or have different characteristics.
 #
@@ -127,9 +127,16 @@ natasha.go_to(rg.Point(-100, 200))
 #
 ###############################################################################
 
+natasha.pen = rg.Pen('blue', 30)
+natasha.right(90)
+natasha.forward(150)
+natasha.backward(75)
+natasha.left(90)
+natasha.forward(100)
+
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       boris    natasha
@@ -154,9 +161,16 @@ natasha.go_to(rg.Point(-100, 200))
 #
 ###############################################################################
 
+hammy = rg.SimpleTurtle('turtle')
+hammy.pen = rg.Pen('khaki', 15)
+hammy.forward(50)
+hammy.left(90)
+hammy.forward(50)
+
+
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
